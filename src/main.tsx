7efef2838b7780893a32eba2f5a7a1767217eb50
caizx <saas_plat@163.com>
2023-06-03 14:05:50 +0800

    fix: slogan

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,15 @@ import 'dayjs/locale/zh-cn';
 import locale from 'antd/locale/zh_CN';
 import './tracker';
 
+console.log(
+  `
+    __ _____ _____ _____ _____ _____ _____ _____ _____ 
+ __|  |     |  _  |   | |   __|  |  |     |  |  |   __|
+|  |  |-   -|     | | | |  |  |  |  |  |  |    -|   __|
+|_____|_____|__|__|_|___|_____|_____|_____|__|__|_____|
+`
+);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ConfigProvider
